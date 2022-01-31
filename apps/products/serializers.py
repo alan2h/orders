c315d167c12025a489d4bec588a57a151999a3dd
Alan Beck <beckalan303@gmail.com>
2022-01-31 12:02:56 -0300

    add validation min_value

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -4,6 +4,9 @@ from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+
+    stock = serializers.IntegerField(min_value=1)
+
     class Meta:
         model = Product
         fields = ['name', 'price', 'stock']
