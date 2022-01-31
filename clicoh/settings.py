19201f9f85c7d4ef10831f0d75582ed0a2fede58
Alan Beck <beckalan303@gmail.com>
2022-01-31 14:04:09 -0300

    change requirements

--- clicoh/settings.py
+++ clicoh/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-enhbzn#6if*&i5$9t^hpc$u+g0q=lba5a9hsz!tb6#$%8kvxrj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
